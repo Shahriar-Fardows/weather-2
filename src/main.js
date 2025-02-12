f15380c6812a914452174b0c846c32a074e97ee9
Sakif <sakifworkspace@gmail.com>
2025-02-13 02:39:42 +0600

    display weather icons conditionally/dynamically

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ const apiKey = "b1ea2c963a3032b8e3e9a9c970cc75a4"
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 
 
 async function checkWeather(city){
@@ -20,7 +21,29 @@ async function checkWeather(city){
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = data.main.temp + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed;
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+
+  if(data.weather[0].main == "Clouds"){
+    console.log(data.weather[0].main)
+    weatherIcon.src = "/src/images/clouds.png"
+  }
+  else if(data.weather[0].main == "Clear"){
+    weatherIcon.src = "/src/images/clear.png"
+
+  }
+  else if(data.weather[0].main == "Rain"){
+    weatherIcon.src = "/src/images/rain.png"
+
+  }
+  else if(data.weather[0].main == "Drizzle"){
+    weatherIcon.src = "/src/images/drizzle.png"
+
+  }
+  else if(data.weather[0].main == "Mist"){
+    weatherIcon.src = "/src/images/mist.png"
+
+  }
 }
 searchBtn.addEventListener("click",()=>{
   checkWeather(searchBox.value)
